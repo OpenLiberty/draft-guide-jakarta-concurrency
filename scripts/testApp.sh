d5d3e5c23d437ee6cd9780701164aed2665d76d2
Gilbert Kwan <gkwan@ca.ibm.com>
2024-08-21 14:07:41 -0400

    fixed test script
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -22,9 +22,16 @@ curl -s http://localhost:9081/api/inventory/systems | grep "\\[\\]" || exit 1
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-     failsafe:integration-test
+    -pl system failsafe:integration-test
+
+mvn -ntp -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl inventory failsafe:integration-test
+
+mvn -ntp -pl system failsafe:verify
+mvn -ntp -pl inventory failsafe:verify
 
 mvn -ntp -pl inventory liberty:stop
 mvn -ntp -pl system liberty:stop
 
-mvn -ntp failsafe:verify
