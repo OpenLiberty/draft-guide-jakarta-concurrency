004018abd52808e9fa326a523ccf6b36df59c280
Gilbert Kwan <gkwan@ca.ibm.com>
2025-06-02 14:37:00 -0400

    update to use maven wrapper (#68)
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testAppFinish.sh
+++ scripts/testAppFinish.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn -ntp liberty:start
+./mvnw -ntp liberty:start
 
 sleep 5
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test
 
-mvn -ntp failsafe:verify
+./mvnw -ntp failsafe:verify
 
-mvn -ntp liberty:stop
+./mvnw -ntp liberty:stop
