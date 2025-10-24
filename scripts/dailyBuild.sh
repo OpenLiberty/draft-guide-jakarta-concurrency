b25130561601cca5f8672493f9e29952e6cd87a8
Gilbert Kwan <gkwan@ca.ibm.com>
2025-10-24 13:28:06 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -21,8 +21,8 @@ if [ "$JDK_LEVEL" == "17" ]; then
 fi
 
 #sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
-sed -i "s;2025-10-23_0302/openliberty-all-25.0.0.12-cl251220251023-0302.zip;$DATE/$DRIVER;g" pom.xml
-cat pom.xml
+sed -i "s;2025-10-23_0302/openliberty-all-25.0.0.12-cl251220251023-0302.zip;$DATE/$DRIVER;g" pom.xml ../start/pom.xml
+cat pom.xml ../start/pom.xml
 
 ../scripts/testAppFinish.sh
 ../scripts/testAppStart.sh
